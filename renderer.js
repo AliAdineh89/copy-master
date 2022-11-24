55d0605245884ac1c67b05165203a75528396207
AdinehAli <ahoora.ali@gmx.at>
2022-11-24 21:06:57 +0100

    style finished and better ux

--- renderer.js
+++ renderer.js
@@ -9,6 +9,16 @@
 const sourceFileBrowserBtn = document.querySelector(".open-source-file-browser");
 const destinationFileBrowserBtn = document.querySelector(".open-destination-file-browser");
 let paths = {}
+let startCopyBtn = document.querySelector('.start-deep-copy')
+let loadingCircle = document.querySelector('.loader')
+
+function initPathValue() {
+    document.getElementById("source-folder").value = localStorage.getItem('srcPath') || ''
+    document.getElementById("destination-folder").value = localStorage.getItem('desPath') || ''
+
+}
+
+initPathValue()
 
 sourceFileBrowserBtn.addEventListener("click", () => {
     window.electronAPI.getDirectoryPath({
@@ -23,23 +33,36 @@ destinationFileBrowserBtn.addEventListener("click", () => {
 })
 
 window.electronAPI.setDirectoryPath((event, response) => {
-
+    if(response.srcPath)localStorage.setItem('srcPath', response.srcPath)
+    if(response.desPath)localStorage.setItem('desPath', response.desPath)
     paths = {
         ...paths,
         ...response,
     };
+    initPathValue()
+    
 })
 
-document.querySelector(".start-deep-copy").addEventListener("click", () => {
-    console.log("==>>", paths)
+
+startCopyBtn.addEventListener("click", () => {
+    
+    startCopyBtn.setAttribute('disabled', true)
+    loadingCircle.style.display = 'block'
     if(paths.srcPath &&  paths.desPath)
     window.electronAPI.startDeepCopy(paths)
     
 })
 
 window.electronAPI.endDeepCopy((event, data) => {
-    console.log("==>>", data)
+     console.log(data)
+    startCopyBtn.removeAttribute('disabled')
+    loadingCircle.style.display = 'none'
+
 })
+
+
+
+
 
 
 
